c2fd15a2aa85738703015db23be940f48d3b0045
陆文轩 <1040152310@qq.com>
2021-03-23 13:25:59 +0800

    在线教育教程项目2021-03-23

--- guli_parent/common/service_base/src/main/java/com/atguigu/servicebase/exceptionhandler/GlobalExceptionHandler.java
+++ guli_parent/common/service_base/src/main/java/com/atguigu/servicebase/exceptionhandler/GlobalExceptionHandler.java
@@ -1,11 +1,13 @@
 package com.atguigu.servicebase.exceptionhandler;
 
 import com.atguigu.commonutils.R;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     //指定出现什么异常执行这个方法
@@ -15,4 +17,14 @@ public class GlobalExceptionHandler {
         e.printStackTrace();
         return R.error().message("执行了全局异常处理..");
     }
+
+    //自定义异常
+    @ExceptionHandler(LwxException.class)
+    @ResponseBody //返回数据
+    public R error(LwxException e){
+        log.error(e.getMessage());
+        e.printStackTrace();
+        return R.error().code(e.getCode()).message(e.getMsg());
+    }
+
 }
---
+++ guli_parent/common/service_base/src/main/java/com/atguigu/servicebase/exceptionhandler/LwxException.java
@@ -0,0 +1,13 @@
+package com.atguigu.servicebase.exceptionhandler;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LwxException extends RuntimeException{
+    private Integer code;
+    private String msg;
+}
